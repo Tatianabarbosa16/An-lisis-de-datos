c18904ad1e0f5fd1553910a9b186c3d026c94f9b
Tatiana Barbosa <tstianabarbosa@gmail.com>
2023-11-11 15:55:11 -0500

    parte 2

--- datos.py
+++ datos.py
@@ -1,16 +1,24 @@
 import numpy as np
+import pandas as pd
 from datasets import load_dataset
 
-
+# Carga  dataset
 dataset = load_dataset("mstz/heart_failure")
 
+# Conversion de la estructura  a  un dataframe en pandas 
+data = pd.DataFrame(dataset["train"])
 
-data = dataset["train"]
+# Separacion del dataframe  en dos  diferentes 
+data_dead = data[data['is_dead'] == 1]
+data_alive = data[data['is_dead'] == 0]
 
-
+# Calculos dedades dat
 ages = np.array(data['age'])
-
-
 average_age = np.mean(ages)
+average_age_dead = np.mean(data_dead['age'])
+average_age_alive = np.mean(data_alive['age'])
 
-print(f"El promedio de edad de las personas participantes en el estudio es {average_age}")
+# Resultados
+print(f"El promedio de edad de las personas participantes en el estudio es {average_age}") 
+print(f"Promedio de edad de las personas que perecieron  en el estudio es {average_age_dead}")
+print(f"Promedio de edad de las personas que sobrevivieron en el estudio es {average_age_alive}") 
